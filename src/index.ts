114b70540607466804cb8c2b35e65ad2fe39f958
hassan <hassanashraf8888@gmail.com>
2025-05-27 16:35:59 +0530

    track user balance in a vault assuming only asset supported

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import { UserBalance } from "../generated/schema";
+import { Address, BigInt, Bytes, ethereum } from "@graphprotocol/graph-ts";
+import { AssetStaked, AssetUnstaked } from "../generated/VaultToken/VaultToken";
+
+export function getUserAssetBalanceId(userAddress: Address): Bytes {
+	return Bytes.fromHexString(userAddress.toHex());
+}
+
+function initUserBalance(userAddress: Address): UserBalance {
+	const userAssetBalanceId = getUserAssetBalanceId(userAddress);
+	let userBalance = new UserBalance(userAssetBalanceId);
+	userBalance.balance = BigInt.fromI32(0);
+	userBalance.updatedAt = BigInt.fromI32(0);
+	return userBalance;
+}
+
+function updateUserBalance(userAddress: Address, amount: BigInt, block: ethereum.Block, isStake: boolean): UserBalance {
+	const userAssetBalanceId = getUserAssetBalanceId(userAddress);
+	let userBalance = UserBalance.load(userAssetBalanceId);
+
+	if (userBalance === null) {
+		userBalance = initUserBalance(userAddress);
+	}
+
+	if (isStake) {
+		userBalance.balance = userBalance.balance.plus(amount);
+	} else {
+		userBalance.balance = userBalance.balance.minus(amount);
+	}
+	userBalance.updatedAt = block.timestamp;
+	userBalance.save();
+	return userBalance;
+}
+
+export function stake(stakeEvent: AssetStaked): void {
+	const userAddress = stakeEvent.params.staker;
+	const amount = stakeEvent.params.amount;
+	updateUserBalance(userAddress, amount, stakeEvent.block, true);
+}
+
+export function unstake(unstakeEvent: AssetUnstaked): void {
+	const userAddress = unstakeEvent.params.staker;
+	const amount = unstakeEvent.params.amount;
+	updateUserBalance(userAddress, amount, unstakeEvent.block, false);
+}
